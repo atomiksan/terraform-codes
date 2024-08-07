e1cd0b9a1057aa642b6ce15d4fdd5fa926749c51
atomiksan <nsofbl@gmail.com>
2024-08-07 09:11:55 +0530

    create and destroy ec2 instances using terraform

--- create-ec2-using-terraform/main.tf
+++ create-ec2-using-terraform/main.tf
@@ -8,3 +8,17 @@ resource "aws_instance" "test" {
   subnet_id = "subnet-031a5c52371f694d6"
   key_name = "ssh_key_pair"
 }
+
+resource "aws_instance" "example" {
+  ami                     = "ami-04a81a99f5ec58529" ## specify appropriate  AMI ID
+  instance_type           = "t2.micro"  
+  subnet_id = "subnet-031a5c52371f694d6"
+  key_name = "ssh_key_pair"
+}
+
+//resource "aws_instance" "bot" {
+//  ami                     = "ami-04a81a99f5ec58529" ## specify appropriate  AMI ID
+//  instance_type           = "t2.micro"  
+//  subnet_id = "subnet-031a5c52371f694d6"
+//  key_name = "ssh_key_pair"
+//}
