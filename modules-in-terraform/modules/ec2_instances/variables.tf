59c3fc4df1fb01938b01e82273257250630c18d6
atomiksan <nsofbl@gmail.com>
2024-08-07 08:39:48 +0530

    creating a variable.tf file for defining variables

---
+++ modules-in-terraform/modules/ec2_instances/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "ami value"
+}
+
+variable "instance_type" {
+  description = "instance type"
+}
+
+variable "subnet_id" {
+  description = "subnet id"
+}
+
+variable "key_name" {
+  description = "login ssh key pair"  
+}
