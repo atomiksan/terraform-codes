60c5deaa6cd93c2473ec307cb6cb9386de9558d9
atomiksan <nsofbl@gmail.com>
2024-08-09 06:22:55 +0530

    ec2 instances modules variables file

---
+++ terraform-workspaces/modules/ec2_instances/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type to be used"
+  type        = string
+}
